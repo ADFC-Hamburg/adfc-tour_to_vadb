cb7b8554c0bb83271b700accd4da0cda54398a19
Sven Anders <sanders@scaleuptech.com>
2023-11-20 11:44:10 +0100

    Update every 12h

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ while : ; do
     python /srv/metroterm/adfc-tour-to-vadb.py
     echo sleep
     find /tmp/adfc* -mtime +1 -delete
-    sleep 20
-    #sleep 43200  # = 12h in seconds
+    #sleep 20
+    sleep 43200  # = 12h in seconds
 done
